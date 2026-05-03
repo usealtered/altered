243066391f507a22c5b8bb6d89cc2a274045c3b0
Riley Barabash <admin@rileybarabash.com>
2026-05-03 17:32:04 -0600

    feat: drizzle kit setup

---
+++ packages/server-experimental/drizzle.config.ts
@@ -0,0 +1,23 @@
+import { dirname, resolve } from "node:path"
+import { fileURLToPath } from "node:url"
+import { getEnvironmentConfig } from "@altered/core-experimental/config/environment"
+import { config } from "dotenv"
+import { defineConfig } from "drizzle-kit"
+
+config({
+    path: resolve(dirname(fileURLToPath(import.meta.url)), "../../.env")
+})
+
+const {
+    shared: {
+        storage: { database }
+    }
+} = getEnvironmentConfig()
+
+export default defineConfig({
+    schema: "./src/**/schema.ts",
+    dialect: "postgresql",
+
+    dbCredentials: { url: database.url },
+    casing: "snake_case"
+})
