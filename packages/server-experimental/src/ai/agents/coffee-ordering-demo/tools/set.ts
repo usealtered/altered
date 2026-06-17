44f7387290f623c6492149368a56f221625bb951
Riley Barabash <admin@rileybarabash.com>
2026-06-17 08:50:28 -0600

    feat: tool set solidification

---
+++ packages/server-experimental/src/ai/agents/coffee-ordering-demo/tools/set.ts
@@ -0,0 +1,55 @@
+import type { Tool, TypedToolCall, TypedToolResult } from "ai"
+import { createOrderCoffeeTool } from "./order-coffee"
+import { createQueryMemoryMockTool } from "./query-memory-mock"
+import { createResearchCoffeeOptionsTool } from "./research-coffee-options"
+import { skipOrderingCoffeeTool } from "./skip-ordering-coffee"
+
+const coffeeOrderingDemoToolNames = [
+    "query-memory",
+
+    "research-coffee-options",
+    "order-coffee",
+    "skip-ordering-coffee"
+] as const
+
+type CoffeeOrderingDemoToolName = (typeof coffeeOrderingDemoToolNames)[number]
+
+/**
+ * Narrow the types of tool names to the types included in the tool set.
+ *
+ * @remarks We may need to prepend `tool-` to the tool call names.
+ */
+function filterCoffeeOrderingDemoToolNames(
+    name: string
+): name is CoffeeOrderingDemoToolName {
+    return coffeeOrderingDemoToolNames.includes(
+        name as CoffeeOrderingDemoToolName
+    )
+}
+
+const coffeeOrderingDemoToolSet = {
+    "query-memory": createQueryMemoryMockTool({
+        include: { brains: ["user"] }
+    }),
+
+    "research-coffee-options": createResearchCoffeeOptionsTool(),
+    "order-coffee": createOrderCoffeeTool(),
+    "skip-ordering-coffee": skipOrderingCoffeeTool
+} satisfies Record<CoffeeOrderingDemoToolName, Tool>
+
+type CoffeeOrderingDemoToolCall = TypedToolCall<
+    typeof coffeeOrderingDemoToolSet
+>
+
+type CoffeeOrderingDemoToolResult = TypedToolResult<
+    typeof coffeeOrderingDemoToolSet
+>
+
+export {
+    type CoffeeOrderingDemoToolCall,
+    type CoffeeOrderingDemoToolName,
+    type CoffeeOrderingDemoToolResult,
+    type coffeeOrderingDemoToolNames,
+    coffeeOrderingDemoToolSet,
+    filterCoffeeOrderingDemoToolNames
+}
