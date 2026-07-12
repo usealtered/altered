4dbec79782b2546653433a9b45224cbf59990af5
Riley Barabash <admin@rileybarabash.com>
2026-07-12 17:11:58 -0600

    feat: launch context resolver

---
+++ packages/raycast-internal-experimental/src/utils/resolve-launch-context.ts
@@ -0,0 +1,25 @@
+import type { LaunchProps } from "@raycast/api"
+import { ArkErrors, type } from "arktype"
+
+function resolveLaunchContext<const Schema>(
+    context: LaunchProps["launchContext"],
+    { schema }: { schema: type.validate<Schema> }
+): type.infer.Out<Schema> | null {
+    const optionalSchema = type.raw(schema)
+
+    const parsedContext = optionalSchema(context)
+
+    if (parsedContext instanceof ArkErrors)
+        console.warn("Invalid launch context, ignoring.", {
+            summary: parsedContext.summary
+        })
+    else if (parsedContext)
+        console.log("Launch context detected:", { context: parsedContext })
+
+    const safeContext =
+        parsedContext instanceof ArkErrors ? null : parsedContext
+
+    return safeContext
+}
+
+export { resolveLaunchContext }
