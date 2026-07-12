f52711c99cdc8d59753edcf56a4c12557a030788
Riley Barabash <admin@rileybarabash.com>
2026-07-12 16:41:54 -0600

    refactor: delete old files that are being rebuilt in package

--- apps/raycast-internal-experimental/src/domains/commands/action-palette/interfaces/collections/definitions.ts
+++
@@ -1,8 +0,0 @@
-import { Grid } from "@raycast/api"
-
-type CollectionLayoutID = "list" | "grid"
-
-const GRID_LAYOUT_COLUMNS = 7
-const GRID_LAYOUT_INSET = Grid.Inset.Large
-
-export { type CollectionLayoutID, GRID_LAYOUT_COLUMNS, GRID_LAYOUT_INSET }
