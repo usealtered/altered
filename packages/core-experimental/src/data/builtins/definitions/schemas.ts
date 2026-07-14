22908f4270406e1b7b7cbac3e8536aedc77ab019
Riley Barabash <admin@rileybarabash.com>
2026-07-14 17:09:45 -0600

    refactor: builtin semantic spacing in formatting

--- packages/core-experimental/src/data/builtins/definitions/schemas.ts
+++ packages/core-experimental/src/data/builtins/definitions/schemas.ts
@@ -6,12 +6,12 @@ import { BUILTIN_THOUGHT_IDS_MAP } from "../ids/thoughts"
 const BUILTIN_SCHEMAS_MAP = {
     INTERFACE_ID: {
         id: BUILTIN_SCHEMA_IDS_MAP.INTERFACE_ID,
+        thoughtId: BUILTIN_THOUGHT_IDS_MAP.INTERFACE_ID_SCHEMA,
 
         type: "thought",
         required: false,
         value: null,
 
-        thoughtId: BUILTIN_THOUGHT_IDS_MAP.INTERFACE_ID_SCHEMA,
         datasetIds: [
             BUILTIN_DATASET_IDS_MAP.ACTIONS,
             BUILTIN_DATASET_IDS_MAP.COLLECTION_INTERFACE_ITEMS
@@ -20,59 +20,59 @@ const BUILTIN_SCHEMAS_MAP = {
 
     INTERFACE_TYPE: {
         id: BUILTIN_SCHEMA_IDS_MAP.INTERFACE_TYPE,
+        thoughtId: BUILTIN_THOUGHT_IDS_MAP.INTERFACE_TYPE_SCHEMA,
 
         type: "text",
         required: true,
         value: null,
 
-        thoughtId: BUILTIN_THOUGHT_IDS_MAP.INTERFACE_TYPE_SCHEMA,
         datasetIds: [BUILTIN_DATASET_IDS_MAP.INTERFACES]
     },
 
     COLLECTION_INTERFACE_ITEM_GROUPS: {
         id: BUILTIN_SCHEMA_IDS_MAP.COLLECTION_INTERFACE_ITEM_GROUPS,
+        thoughtId:
+            BUILTIN_THOUGHT_IDS_MAP.COLLECTION_INTERFACE_ITEM_GROUPS_SCHEMA,
 
         type: "dataset",
         required: false,
         value: null,
 
-        thoughtId:
-            BUILTIN_THOUGHT_IDS_MAP.COLLECTION_INTERFACE_ITEM_GROUPS_SCHEMA,
         datasetIds: [BUILTIN_DATASET_IDS_MAP.COLLECTION_INTERFACES]
     },
 
     COLLECTION_INTERFACE_ITEM_GROUP_TITLE: {
         id: BUILTIN_SCHEMA_IDS_MAP.COLLECTION_INTERFACE_ITEM_GROUP_TITLE,
+        thoughtId:
+            BUILTIN_THOUGHT_IDS_MAP.COLLECTION_INTERFACE_ITEM_GROUP_TITLE_SCHEMA,
 
         type: "text",
         required: false,
         value: null,
 
-        thoughtId:
-            BUILTIN_THOUGHT_IDS_MAP.COLLECTION_INTERFACE_ITEM_GROUP_TITLE_SCHEMA,
         datasetIds: [BUILTIN_DATASET_IDS_MAP.COLLECTION_INTERFACE_ITEM_GROUPS]
     },
 
     COLLECTION_INTERFACE_ITEM_GROUP_SUBTITLE: {
         id: BUILTIN_SCHEMA_IDS_MAP.COLLECTION_INTERFACE_ITEM_GROUP_SUBTITLE,
+        thoughtId:
+            BUILTIN_THOUGHT_IDS_MAP.COLLECTION_INTERFACE_ITEM_GROUP_SUBTITLE_SCHEMA,
 
         type: "text",
         required: false,
         value: null,
 
-        thoughtId:
-            BUILTIN_THOUGHT_IDS_MAP.COLLECTION_INTERFACE_ITEM_GROUP_SUBTITLE_SCHEMA,
         datasetIds: [BUILTIN_DATASET_IDS_MAP.COLLECTION_INTERFACE_ITEM_GROUPS]
     },
 
     COLLECTION_INTERFACE_ITEMS: {
         id: BUILTIN_SCHEMA_IDS_MAP.COLLECTION_INTERFACE_ITEMS,
+        thoughtId: BUILTIN_THOUGHT_IDS_MAP.COLLECTION_INTERFACE_ITEMS_SCHEMA,
 
         type: "dataset",
         required: false,
         value: null,
 
-        thoughtId: BUILTIN_THOUGHT_IDS_MAP.COLLECTION_INTERFACE_ITEMS_SCHEMA,
         datasetIds: [
             BUILTIN_DATASET_IDS_MAP.COLLECTION_INTERFACES,
             BUILTIN_DATASET_IDS_MAP.COLLECTION_INTERFACE_ITEM_GROUPS
@@ -81,52 +81,52 @@ const BUILTIN_SCHEMAS_MAP = {
 
     COLLECTION_INTERFACE_ITEM_ICON: {
         id: BUILTIN_SCHEMA_IDS_MAP.COLLECTION_INTERFACE_ITEM_ICON,
+        thoughtId:
+            BUILTIN_THOUGHT_IDS_MAP.COLLECTION_INTERFACE_ITEM_ICON_SCHEMA,
 
         type: "text",
         required: false,
         value: null,
 
-        thoughtId:
-            BUILTIN_THOUGHT_IDS_MAP.COLLECTION_INTERFACE_ITEM_ICON_SCHEMA,
         datasetIds: [BUILTIN_DATASET_IDS_MAP.COLLECTION_INTERFACE_ITEMS]
     },
 
     COLLECTION_INTERFACE_ITEM_TITLE: {
         id: BUILTIN_SCHEMA_IDS_MAP.COLLECTION_INTERFACE_ITEM_TITLE,
+        thoughtId:
+            BUILTIN_THOUGHT_IDS_MAP.COLLECTION_INTERFACE_ITEM_TITLE_SCHEMA,
 
         type: "text",
         required: false,
         value: null,
 
-        thoughtId:
-            BUILTIN_THOUGHT_IDS_MAP.COLLECTION_INTERFACE_ITEM_TITLE_SCHEMA,
         datasetIds: [BUILTIN_DATASET_IDS_MAP.COLLECTION_INTERFACE_ITEMS]
     },
 
     COLLECTION_INTERFACE_ITEM_SUBTITLE: {
         id: BUILTIN_SCHEMA_IDS_MAP.COLLECTION_INTERFACE_ITEM_SUBTITLE,
+        thoughtId:
+            BUILTIN_THOUGHT_IDS_MAP.COLLECTION_INTERFACE_ITEM_SUBTITLE_SCHEMA,
 
         type: "text",
         required: false,
         value: null,
 
-        thoughtId:
-            BUILTIN_THOUGHT_IDS_MAP.COLLECTION_INTERFACE_ITEM_SUBTITLE_SCHEMA,
         datasetIds: [BUILTIN_DATASET_IDS_MAP.COLLECTION_INTERFACE_ITEMS]
     },
 
     COLLECTION_INTERFACE_ITEM_TRIGGER_PHRASE: {
         id: BUILTIN_SCHEMA_IDS_MAP.COLLECTION_INTERFACE_ITEM_TRIGGER_PHRASE,
+        thoughtId:
+            BUILTIN_THOUGHT_IDS_MAP.COLLECTION_INTERFACE_ITEM_TRIGGER_PHRASE_SCHEMA,
 
         type: "text",
         required: false,
         value: null,
 
-        thoughtId:
-            BUILTIN_THOUGHT_IDS_MAP.COLLECTION_INTERFACE_ITEM_TRIGGER_PHRASE_SCHEMA,
         datasetIds: [BUILTIN_DATASET_IDS_MAP.COLLECTION_INTERFACE_ITEMS]
     }
-} as const satisfies Record<string, ALTEREDSchema>
+} as const satisfies Record<keyof typeof BUILTIN_SCHEMA_IDS_MAP, ALTEREDSchema>
 
 const BUILTIN_SCHEMAS = Object.values(BUILTIN_SCHEMAS_MAP)
 
